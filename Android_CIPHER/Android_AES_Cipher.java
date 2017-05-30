efc597e61f7adbf54215019085e7a9ac912fcf16
chaudhuri-ab <chaudhu1@purdue.edu>
2017-05-29 20:20:53 -0500

    Added Android Ciphers

---
+++ Android_CIPHER/Android_AES_Cipher.java
@@ -0,0 +1,88 @@
+package ciphers;
+
+        import javax.crypto.Cipher;
+        import javax.crypto.spec.IvParameterSpec;
+        import javax.crypto.spec.SecretKeySpec;
+
+        import android.util.Base64;
+
+public class Android_AES_Cipher {
+
+    private static String CIPHER_NAME = "AES/CBC/PKCS5PADDING";
+    private static int CIPHER_KEY_LEN = 16; //128 bits
+
+    /**
+     * Encrypt data using AES Cipher (CBC) with 128 bit key
+     *
+     *
+     * @param key  - key to use should be 16 bytes long (128 bits)
+     * @param iv - initialization vector
+     * @param data - data to encrypt
+     * @return encryptedData data in base64 encoding with iv attached at end after a :
+     */
+    public static String encrypt(String key, String iv, String data) {
+        try {
+            if (key.length() < Java_AES_Cipher.CIPHER_KEY_LEN) {
+                int numPad = Java_AES_Cipher.CIPHER_KEY_LEN - key.length();
+
+                for(int i = 0; i < numPad; i++){
+                    key += "0"; //0 pad to len 16 bytes
+                }
+
+            } else if (key.length() > Java_AES_Cipher.CIPHER_KEY_LEN) {
+                key = key.substring(0, CIPHER_KEY_LEN); //truncate to 16 bytes
+            }
+
+
+            IvParameterSpec initVector = new IvParameterSpec(iv.getBytes("UTF-8"));
+            SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes("UTF-8"), "AES");
+
+            Cipher cipher = Cipher.getInstance(Java_AES_Cipher.CIPHER_NAME);
+            cipher.init(Cipher.ENCRYPT_MODE, skeySpec, initVector);
+
+            byte[] encryptedData = cipher.doFinal((data.getBytes()));
+
+            String base64_EncryptedData = new String(Base64.encodeToString(encryptedData, Base64.DEFAULT));
+            String base64_IV = new String(Base64.encodeToString(iv.getBytes("UTF-8"), Base64.DEFAULT));
+
+            return base64_EncryptedData + ":" + base64_IV;
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        return null;
+    }
+
+    /**
+     * Decrypt data using AES Cipher (CBC) with 128 bit key
+     *
+     * @param key - key to use should be 16 bytes long (128 bits)
+     * @param data - encrypted data with iv at the end separate by :
+     * @return decrypted data string
+     */
+
+    public static String decrypt(String key, String data) {
+        try {
+
+            String[] parts = data.split(":");
+
+            IvParameterSpec iv = new IvParameterSpec(Base64.decode(parts[1], Base64.DEFAULT));
+            SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes("UTF-8"), "AES");
+
+            Cipher cipher = Cipher.getInstance(Java_AES_Cipher.CIPHER_NAME);
+            cipher.init(Cipher.DECRYPT_MODE, skeySpec, iv);
+
+            byte[] decodedEncryptedData = Base64.decode(parts[0], Base64.DEFAULT);
+
+            byte[] original = cipher.doFinal(decodedEncryptedData);
+
+            return new String(original);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        return null;
+    }
+
+}
